58a43f7a3f57875945821db8c7b9b0f69e6ec269
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2026-02-27 17:46:55 +0200

    deobfuscate: Unit tests

--- NuciText.Obfuscation.UnitTests/NuciTextObfuscatorTests.cs
+++ NuciText.Obfuscation.UnitTests/NuciTextObfuscatorTests.cs
@@ -1,38 +1,57 @@
-using NUnit.Framework;
-
 namespace NuciText.Obfuscation.UnitTests
 {
     public class NuciTextObfuscatorTests
     {
-        static int StaticTestSeed => 123456789;
-        static string StaticTestString => "Test string!";
+        static int TestSeed => 123456789;
+        static string TestPlainString => "Test string!";
+        static string TestObfuscatedString => "ꓔеst strіng!";
 
         INuciTextObfuscator obfuscator;
 
         [SetUp]
         public void Setup()
         {
-            obfuscator = new NuciTextObfuscator(StaticTestSeed);
+            obfuscator = new NuciTextObfuscator(TestSeed);
         }
+
+        [Test]
+        public void GivenAnEmptyString_WhenDebfuscating_ThenTheResultIsEmpty()
+            => Assert.That(obfuscator.Deobfuscate(string.Empty), Is.Empty);
 
         [Test]
         public void GivenAnEmptyString_WhenObfuscating_ThenTheResultIsEmpty()
             => Assert.That(obfuscator.Obfuscate(string.Empty), Is.Empty);
 
         [Test]
+        public void GivenANullString_WhenDeobfuscating_ThenTheResultIsNull()
+            => Assert.That(obfuscator.Deobfuscate(null), Is.Null);
+
+        [Test]
         public void GivenANullString_WhenObfuscating_ThenTheResultIsNull()
             => Assert.That(obfuscator.Obfuscate(null), Is.Null);
 
         [Test]
+        public void GivenAValidString_WhenDeobfuscating_ThenTheResultIsNotNull()
+            => Assert.That(obfuscator.Deobfuscate(TestObfuscatedString), Is.Not.Null);
+
+        [Test]
         public void GivenAValidString_WhenObfuscating_ThenTheResultIsNotNull()
-            => Assert.That(obfuscator.Obfuscate(StaticTestString), Is.Not.Null);
+            => Assert.That(obfuscator.Obfuscate(TestPlainString), Is.Not.Null);
+
+        [Test]
+        public void GivenAValidString_WhenDeobfuscating_ThenTheResultIsNotEmpty()
+            => Assert.That(obfuscator.Deobfuscate(TestObfuscatedString), Is.Not.Empty);
 
         [Test]
         public void GivenAValidString_WhenObfuscating_ThenTheResultIsNotEmpty()
-            => Assert.That(obfuscator.Obfuscate(StaticTestString), Is.Not.Empty);
+            => Assert.That(obfuscator.Obfuscate(TestPlainString), Is.Not.Empty);
+
+        [Test]
+        public void GivenAValidString_WhenDeobfuscating_ThenTheResultHasBeenDeobfuscated()
+            => Assert.That(obfuscator.Deobfuscate(TestObfuscatedString), Is.EqualTo(TestPlainString));
 
         [Test]
         public void GivenAValidString_WhenObfuscating_ThenTheResultHasBeenObfuscated()
-            => Assert.That(obfuscator.Obfuscate(StaticTestString), Is.Not.EqualTo(StaticTestString));
+            => Assert.That(obfuscator.Obfuscate(TestPlainString), Is.EqualTo(TestObfuscatedString));
     }
 }
