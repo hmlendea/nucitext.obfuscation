2b56eb8637616fc71be46023adf71b89330adb92
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2026-02-27 18:10:45 +0200

    doc: Added code documentation

--- NuciText.Obfuscation/NuciTextObfuscatorOptions.cs
+++ NuciText.Obfuscation/NuciTextObfuscatorOptions.cs
@@ -1,7 +1,13 @@
 namespace NuciText.Obfuscation
 {
+    /// <summary>
+    /// Defines the options for the NuciText obfuscator, allowing customisation of the obfuscation process.
+    /// </summary>
     public sealed class NuciTextObfuscatorOptions
     {
+        /// <summary>
+        /// Gets or sets a value indicating whether to use approximate replacements for characters.
+        /// </summary>
         public bool UseApproximateReplacements { get; set; } = false;
     }
 }
